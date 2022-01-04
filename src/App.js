8e38e27ce865d14b706c1655a01aaed003f0bfa1
Faizaan shaikh <sfaizan974@gmail.com>
2022-01-04 22:46:33 +0530

    Bugs Fixed

--- src/App.js
+++ src/App.js
@@ -2,20 +2,9 @@ import './App.css';
 import CounterContainer from './Components/CounterContainer';
 
 function App() {
+
   return (
     <>
-      {/* <div className="mainContainer">
-        <div className="container">
-          <h1 >Counter App</h1>
-          <div className="CounterNumberContainer">
-            <h2>0</h2>
-          </div>
-          <div className="buttonsContainer">
-            <button id='incrementButton'>Increment</button>
-            <button id='decrementButton'>Decrement</button>
-          </div>
-        </div>
-      </div> */}
       <CounterContainer />
     </ >
   );
--- src/Components/CounterContainer.js
+++ src/Components/CounterContainer.js
@@ -1,19 +1,54 @@
 import React from 'react'
 import './CounterContainer.css'
+import { useState } from 'react'
 
-export default function counterContainer() {
+function CounterContainer() {
+
+    const [Number, setNumber] = useState(0)
+    const [ColorValue, setColorValue] = useState('black')
+
+    const verifyNumber = (num) => {
+        if (num > 0) {
+            setColorValue('green')
+        } else if (num === 0) {
+            setColorValue('black')
+        } else {
+            setColorValue('red')
+        }
+    }
+
+    const handleIncrement = () => {
+        let newNumber = Number + 1
+        setNumber(newNumber)
+        console.log(newNumber)
+        verifyNumber(newNumber)
+        // console.log("i am Increment")
+    }
+
+    const handleDecrement = () => {
+        let newNumber = Number - 1
+        setNumber(newNumber)
+        verifyNumber(newNumber)
+        // console.log("i am Decrement")
+    }
+
     return (
-        <div className="mainContainer">
-            <div className="container">
-                <h1 >Counter App</h1>
-                <div className="CounterNumberContainer">
-                    <h2>0</h2>
-                </div>
-                <div className="buttonsContainer">
-                    <button id='incrementButton'>Increment</button>
-                    <button id='decrementButton'>Decrement</button>
+        <>
+            <div className="mainContainer">
+                <div className="container">
+                    <h1 >Counter App</h1>
+                    <div className="CounterNumberContainer">
+                        {/* <h2 style={{ color: { ColorValue } }}>{Number}</h2> */}
+                        <h2 style={{ color: ColorValue }}>{Number}</h2>
+                    </div>
+                    <div className="buttonsContainer">
+                        <button id='incrementButton' onClick={handleIncrement}>Increment</button>
+                        <button id='decrementButton' onClick={handleDecrement}>Decrement</button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
+
+export default CounterContainer
